1e0de51135e7754a48dd6d1cc86a6c6463ecc7c6
Asif Sheriff <asif.sheriff77@gmail.com>
2024-08-10 10:59:22 +0530

    dahsboard update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
-import './index.css';  // Import your CSS here if you're using Tailwind or any other styles
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
